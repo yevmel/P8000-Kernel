d8285b75da56de24cb89b4f8e78aae73a4275b7d
Yevgeniy Melnichuk <yevgeniy.melnichuk@googlemail.com>
2015-10-06 19:36:20 +0000

    fixed include of jpeg_cmdq.h

--- drivers/misc/mediatek/jpeg/mt6735/jpeg_cmdq.c
+++ drivers/misc/mediatek/jpeg/mt6735/jpeg_cmdq.c
@@ -1,4 +1,4 @@
-#include <jpeg_cmdq.h>
+#include "jpeg_cmdq.h"
 #include <cmdq_core.h>
 #include <cmdq_reg.h>
 
@@ -8,7 +8,7 @@
 
 int32_t cmdqJpegClockOn(uint64_t engineFlag)
 {
-#if 0 
+#if 0
     if (engineFlag & (1 << CMDQ_ENG_JPEG_DEC)){
       enable_clock(MT65XX_PDN_MM_JPEG_DEC,"JPEG");
     }
@@ -18,7 +18,7 @@ int32_t cmdqJpegClockOn(uint64_t engineFlag)
     if (engineFlag & (1 << CMDQ_ENG_JPEG_ENC_PASS2)){
       enable_clock(MT65XX_PDN_MM_JPEG_DEC,"JPEG");
     }
-#endif    
+#endif
     return 0;
 }
 
@@ -34,21 +34,21 @@ int32_t cmdqJpegDumpInfo(uint64_t engineFlag,
 int32_t cmdqJpegResetEng(uint64_t engineFlag)
 {
     if (engineFlag & (1 << CMDQ_ENG_JPEG_DEC)){
-       IMG_REG_WRITE(0x0000FFFF, REG_ADDR_JPGDEC_INTERRUPT_STATUS); //ack decoder 
+       IMG_REG_WRITE(0x0000FFFF, REG_ADDR_JPGDEC_INTERRUPT_STATUS); //ack decoder
        IMG_REG_WRITE(0x00, REG_ADDR_JPGDEC_RESET); //REG_JPGDEC_RESET = 0x00;
        IMG_REG_WRITE(0x01, REG_ADDR_JPGDEC_RESET); //REG_JPGDEC_RESET = 0x01;
-       
-       IMG_REG_WRITE(0x00, REG_ADDR_JPGDEC_RESET);  //REG_JPGDEC_RESET = 0x00;    
+
+       IMG_REG_WRITE(0x00, REG_ADDR_JPGDEC_RESET);  //REG_JPGDEC_RESET = 0x00;
        IMG_REG_WRITE(0x10, REG_ADDR_JPGDEC_RESET);  //REG_JPGDEC_RESET = 0x10;
     }
-   
-    if (engineFlag & (1 << CMDQ_ENG_JPEG_ENC)){    
-      IMG_REG_WRITE( 0, REG_ADDR_JPEG_ENC_RSTB) ;  
-      IMG_REG_WRITE( 1, REG_ADDR_JPEG_ENC_RSTB) ;  
+
+    if (engineFlag & (1 << CMDQ_ENG_JPEG_ENC)){
+      IMG_REG_WRITE( 0, REG_ADDR_JPEG_ENC_RSTB) ;
+      IMG_REG_WRITE( 1, REG_ADDR_JPEG_ENC_RSTB) ;
     }
-	 
-	 if (engineFlag & (1 << CMDQ_ENG_JPEG_REMDC)){
-      IMG_REG_WRITE( 0, REG_ADDR_JPEG_ENC_PASS2_RSTB) ;  
+
+     if (engineFlag & (1 << CMDQ_ENG_JPEG_REMDC)){
+      IMG_REG_WRITE( 0, REG_ADDR_JPEG_ENC_PASS2_RSTB) ;
       IMG_REG_WRITE( 1, REG_ADDR_JPEG_ENC_PASS2_RSTB) ;
     }
 
@@ -60,16 +60,16 @@ int32_t cmdqJpegResetEng(uint64_t engineFlag)
 
 int32_t cmdqJpegClockOff(uint64_t engineFlag)
 {
-   
-#if 0    
+
+#if 0
     if (engineFlag & (1 << CMDQ_ENG_JPEG_DEC)){
-      disable_clock(MT65XX_PDN_MM_JPEG_DEC,"JPEG");  
+      disable_clock(MT65XX_PDN_MM_JPEG_DEC,"JPEG");
     }
     if (engineFlag & (1 << CMDQ_ENG_JPEG_ENC_PASS1)){
-      disable_clock(MT65XX_PDN_MM_JPEG_DEC,"JPEG");  
+      disable_clock(MT65XX_PDN_MM_JPEG_DEC,"JPEG");
     }
     if (engineFlag & (1 << CMDQ_ENG_JPEG_ENC_PASS2)){
-      disable_clock(MT65XX_PDN_MM_JPEG_DEC,"JPEG");  
+      disable_clock(MT65XX_PDN_MM_JPEG_DEC,"JPEG");
     }
 #endif
     return 0;
